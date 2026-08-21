cad7365761f775a19c0b91cd7e44705ee0e0581b
Herlyck <mrcyrax64@gmail.com>
2026-08-21 14:26:04 -0300

    base de datos

---
+++ Base.sql
@@ -0,0 +1,21 @@
+drop database if kiosco exists;
+create database Kiosco;
+use kiosco;
+
+create table golosinas(
+    id int primary key auto_increment,
+    nombre varchar(50) not null,
+    marca varchar(50) not null
+
+);
+
+insert into golosinas (nombre,marca) values ("Alfajor","Fantoche");
+insert into golosinas (nombre,marca) values ("Galletitas","Arcor");
+insert into golosinas (nombre,marca) values ("Galletitas","Bagley");
+insert into golosinas (nombre,marca) values ("Galletitas","Carrefour");
+insert into golosinas (nombre,marca) values ("Galletitas","Dia");
+insert into golosinas (nombre,marca) values ("Galletitas","Oreo");
+
+delete from golosinas where id = 3;
+
+update golosinas set nombre = "Papas Fritas", marca="Lays" where id = 2;
